fe987f8043e6f7bfc001f6a7d661ad6df7a4a4a1
Felipe <jsfelipe@gmail.com>
2024-08-29 21:07:00 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/fonts/*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-        ],
-      },
-    ];
-  },
+  }
 }
